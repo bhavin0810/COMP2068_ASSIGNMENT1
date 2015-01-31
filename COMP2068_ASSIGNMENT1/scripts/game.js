cd77364212642d6752efba2ea2ad053c45268809
bhavin0810 <bhavin0810@gmail.com>
2015-01-30 20:52:47 -0500

    Code Commented Properly

--- COMP2068_ASSIGNMENT1/scripts/game.js
+++ COMP2068_ASSIGNMENT1/scripts/game.js
@@ -1,6 +1,11 @@
-﻿/// <reference path="lib/impress.js" />
+﻿/*
+@File_Name : game.css
+@author : Bhavin Patel
+@Description: JavaScript for the game
+@Version 1.0 Jan-29-2015 game.js added and add reference for the impress.js
+*/
 
-
+/// <reference path="lib/impress.js" />
 
 function main() {
     console.log("Game Started");
